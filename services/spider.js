3d56075e2d60c96e35ac155cbce08a09997ac209
luoweihua7 <luoweihua7@gmail.com>
2017-02-09 17:00:49 +0800

    Initialize

---
+++ services/spider.js
@@ -0,0 +1,51 @@
+/**
+ * Spider
+ *
+ * let spider = require('./spider');
+ * let adapter = require('./adapter');
+ * let sqlite = require('./sqlite');
+ * let redis  = require('./redis');
+ * spider.use(adapter);
+ * spider.use(sqlite);
+ * spider.use(redis);
+ * spider.likes('some blog name');
+ */
+let tumblr = require('./tumblr');
+let sqlite = require('./sqlite');
+let tasks = require('../modules/tasks');
+
+function Spider() {
+    let self = this;
+    let callbacks = [];
+
+    function handle(data) {
+        callbacks.forEach(fn => {
+            fn(data);
+        });
+    }
+
+    this.use = plugin => {
+        callbacks.push(plugin);
+    };
+
+    this.grab = (blog, type) => {
+        let task = tumblr[type](blog);
+        task.on('data', handle);
+
+        return {};
+    };
+
+    this.likes = blog => {
+        return self.grab(blog, 'likes');
+    };
+
+    this.following = blog => {
+        return self.grab(blog, 'following');
+    }
+
+    this.posts = blog => {
+        return self.grab(blog, 'posts');
+    }
+}
+
+module.exports = new Spider();
--- services/tumblr.js
+++ services/tumblr.js
@@ -2,14 +2,14 @@ let tumblr = require('../modules/tumblr');
 let tasks = require('../modules/tasks');
 let Grab = require('../modules/grab');
 
-exports.userLikes = function(blog) {
+exports.likes = function(blog) {
     return new Grab(blog, 'likes');
 };
 
-exports.userFollowing = function(blog) {
+exports.following = function(blog) {
     return new Grab(blog, 'following');
 };
 
-exports.userPosts = function(blog) {
+exports.posts = function(blog) {
     return new Grab(blog, 'posts');
 };
